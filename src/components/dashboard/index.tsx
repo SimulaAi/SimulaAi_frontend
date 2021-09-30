d8d30a72f5d408bfb95758078521d0f2b28113fc
okatsuo <rafael.goncanna@gmail.com>
2021-09-30 20:50:34 -0300

    user login e logout

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,9 +1,22 @@
+import Router from 'next/router'
+import { UserLogout } from '../../utils/authentication'
+import { Container } from '../container'
+import * as Styles from './styles'
+
 const Dashboard = () => {
+  const handleLogout = async () => {
+    UserLogout()
+    await Router.push('/')
+  }
   return (
-    <div>
-      <p>ainda em desenvolvimento!</p>
-      <button className="success">sair</button>
-    </div>
+    <Styles.Wrapper>
+      <Container>
+        <Styles.Content>
+        <h1 style={{ marginBottom: '80px' }}>ainda em desenvolvimento!</h1>
+        <button onClick={handleLogout} className="btn btn-danger"><h1>sair</h1></button>
+        </Styles.Content>
+      </Container>
+    </Styles.Wrapper>
   )
 }
 
