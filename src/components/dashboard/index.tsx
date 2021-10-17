d9fca3dd2b68bd77509dda391b291cdebfd3e2ed
okatsuo <rafael.goncanna@gmail.com>
2021-10-17 19:55:25 -0300

    feat: remove unnecessary container from dashboard

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,15 +1,12 @@
-import { Container } from '../container'
 import * as Styles from './styles'
 
 const Dashboard = () => {
   return (
     <Styles.Wrapper>
-      <Container>
         <Styles.Content>
         <h1 style={{ marginBottom: '80px' }}>HOME PAGE</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In unde beatae laboriosam reprehenderit iure amet aliquam vero reiciendis. Praesentium repellat, aut suscipit saepe numquam blanditiis quas nihil atque autem expedita?</p>
         </Styles.Content>
-      </Container>
     </Styles.Wrapper>
   )
 }
