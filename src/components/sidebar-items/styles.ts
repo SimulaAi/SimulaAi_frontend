a3c5b209c6da30633f3020b4c4b3958689d6f3fa
okatsuo <rafael.goncanna@gmail.com>
2021-10-17 21:04:30 -0300

    refactor: remove unnecessary class

--- src/components/sidebar-items/styles.ts
+++ src/components/sidebar-items/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+height: 100%;
+`
 
 export const Content = styled.div`
   display: flex;
@@ -15,9 +17,4 @@ export const Item = styled.div`
   width: 80px;
   padding: 20px 10px;
   cursor: pointer;
-`
-
-export const ShutDown = styled.div`
-  width: 80px;
-  padding: 20px 10px;
 `
